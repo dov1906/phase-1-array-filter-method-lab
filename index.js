2b981d5664d5e16de184b1a06d6ffcf3a07593b8
Dov Azogui <dov.azogui@gmail.com>
2024-08-29 17:04:45 -0400

    aaa

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, string){
+
+
+    const check = array.filter(element => element.toLowerCase() === string.toLowerCase()  )
+
+    return check
+}
+
+
+function fuzzyMatch(array, string) {
+    const check = array.filter(element => element.slice(0,string.length ) === string) 
+    return check
+}
+
+
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "N/A"},
+    { year: "2009", result: "N/A"},
+    { year: "2008", result: "N/A"}, 
+]
+
+
+
+function matchName(array, string) {
+    const check = array.filter(element => element.name === string)
+    return check
+}
+
+console.log(matchName(record, "2009"))
+
